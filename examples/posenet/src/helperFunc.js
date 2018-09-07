f19485efa615f82824ef14e219e29dd4cbce0b3b
Ningxin Hu <ningxin.hu@intel.com>
2018-09-07 17:53:11 +0800

    PoseNet uses imported models

--- examples/posenet/src/helperFunc.js
+++ examples/posenet/src/helperFunc.js
@@ -28,7 +28,7 @@ function getInput(inputElement) {
 
 function getURL(version) {
   let address;
-  const urlBase = 'https://storage.googleapis.com/tfjs-models/weights/posenet/';
+  const urlBase = './models/';
   switch (version) {
     case 1.01:
       address = urlBase + 'mobilenet_v1_101/';
